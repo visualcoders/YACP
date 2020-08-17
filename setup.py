e33633485e20e1417d3cdd949e18c90a4f677648
adas12w <36375435+adas12w@users.noreply.github.com>
2020-08-17 18:19:35 +0300

    Organized file structure

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'YACL',         # How you named your package folder (MyLib)
+  packages = ['YACL'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Yet Another Crawler Library. You can use it by a generator or by creating an instance',
+  author = 'Ada BERK',                   # Type in your name
+  author_email = 'adaberk.kth@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
