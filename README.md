ff36613b343dd257a1d70d04042c316e26f777ba
adas12w <36375435+adas12w@users.noreply.github.com>
2020-08-17 18:39:01 +0300

    Fixed naming issues

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# YACL
+# YACP
 
-Yet another crawling library. 
+Yet another crawling package. 
 It's zero dependency package for python that helps you to crawl into a directory.
 You can use a context manager or an instance and use.
 It based on generators so you can crawl into larger content.
 
-Thanks for using YACL.
+Thanks for using YACP.
 If you have any questions you can send an email adaberk.kth@gmail.com
--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from distutils.core import setup
+
 setup(
-  name = 'YACL',         # How you named your package folder (MyLib)
-  packages = ['YACL'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Yet Another Crawler Library. You can use it by a generator or by creating an instance',
-  author = 'Ada BERK',                   # Type in your name
-  author_email = 'adaberk.kth@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-      ],
+  name = 'yacp',
+  packages = ['yacp'],
+  version = '0.1',
+  license='MIT',
+  description = 'Yet Another Crawler Package. A simple directory crawler. You can use it by a generator or by creating an instance',
+  author = 'Ada BERK',
+  author_email = 'adaberk.kth@gmail.com',
+  url='https://github.com/visualcoders/YACP',
+  download_url='https://github.com/visualcoders/YACP/archive/v0.1.tar.gz',
+  keywords = ['Crawling', 'Directory', 'Library', 'Package', 'Simple', 'Generator', 'Context Manager', 'Variable', 'Helper'],  
   classifiers=[
     'Development Status :: 4 - Beta',      
     'Intended Audience :: Developers',      
