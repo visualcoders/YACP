d9d5d7f6fd081742e44784d3565d75e70b409a63
adas12w <36375435+adas12w@users.noreply.github.com>
2020-08-17 19:02:16 +0300

    Version update 2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'yacp',
   packages = ['yacp'],
-  version = '0.1',
+  version = '0.2',
   license='MIT',
   description = 'Yet Another Crawler Package. A simple directory crawler. You can use it by a generator or by creating an instance',
   long_description="A directory crawler based on generators. Using generators slow down the process a bit, but fixes memory issues with that way you can crawl into large files",
