bb125fc0ff310eca388942ef9a68f2326c0c78eb
adas12w <36375435+adas12w@users.noreply.github.com>
2020-08-17 18:46:47 +0300

    Changed setup.py
    With that way it'll auto-update the long description

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from distutils.core import setup
+from os import path
+
+long_description=str()
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
   name = 'yacp',
@@ -6,6 +13,8 @@ setup(
   version = '0.1',
   license='MIT',
   description = 'Yet Another Crawler Package. A simple directory crawler. You can use it by a generator or by creating an instance',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   author = 'Ada BERK',
   author_email = 'adaberk.kth@gmail.com',
   url='https://github.com/visualcoders/YACP',
@@ -20,5 +29,6 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
   ],
 )
